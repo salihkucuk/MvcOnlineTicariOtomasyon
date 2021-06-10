0ec11dbc94d9d23eb916ac4ffd2863aa23f02e56
Salih Küçük <salihkucuk0@hotmail.com>
2021-06-10 11:02:24 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon1/Controllers/FaturaController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon1.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon1.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var liste = c.Faturalars.ToList();
+            return View(liste);
+        }
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FaturaEkle(Faturalar f)
+        {
+            c.Faturalars.Add(f);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult FaturaGetir(int id)
+        {
+            var fatura = c.Faturalars.Find(id);
+            return View("FaturaGetir", fatura);
+        }
+        public ActionResult FaturaGuncelle(Faturalar f)
+        {
+            var fat = c.Faturalars.Find(f.FaturaID);
+            fat.FaturaSeriNo = f.FaturaSeriNo;
+            fat.FaturaSıraNo = f.FaturaSıraNo;
+            fat.VergiDairesi = f.VergiDairesi;
+            fat.Tarih = f.Tarih;
+            fat.Saat = f.Saat;
+            fat.TeslimEden = f.TeslimEden;
+            fat.TeslimAlan = f.TeslimAlan;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult FaturaDetay(int id)
+        {
+            var degerler = c.FaturaKalems.Where(x => x.FaturaID == id).ToList();
+
+            var dpt = c.Faturalars.Where(x => x.FaturaID == id).Select(y =>y.FaturaSeriNo+" "+y.FaturaSıraNo).FirstOrDefault();
+            ViewBag.f = dpt;
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKalem()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKalem(FaturaKalem p)
+        {
+            c.FaturaKalems.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Dinamik()
+        {
+            Class3 cs = new Class3();
+            cs.deger1 = c.Faturalars.ToList();
+            cs.deger2 = c.FaturaKalems.ToList();
+            return View(cs);
+        }
+        public ActionResult FaturayıKaydet(string FaturaSeriNo,string FaturaSıraNo,DateTime Tarih,string VergiDairesi,string Saat,
+            string TeslimEden,string TeslimAlan,string Toplam,FaturaKalem[] kalemler)
+        {
+            Faturalar f = new Faturalar();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSıraNo = FaturaSıraNo;
+            f.Tarih = Tarih;
+            f.VergiDairesi = VergiDairesi;
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam = decimal.Parse(Toplam);
+            c.Faturalars.Add(f);
+            foreach(var x in kalemler)
+            {
+                FaturaKalem fk = new FaturaKalem();
+                fk.Aaciklama = x.Aaciklama;
+                fk.BirimFiyat = x.BirimFiyat;
+                fk.FaturaID = x.FaturaID;
+                fk.Miktar = x.Miktar;
+                fk.Tutar = x.Tutar;
+                c.FaturaKalems.Add(fk);
+            }
+            c.SaveChanges();
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon1/Models/Siniflar/Class3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon1.Models.Siniflar
+{
+    public class Class3
+    {
+        public IEnumerable<Faturalar> deger1 { get; set; }
+        public IEnumerable<FaturaKalem> deger2 { get; set; }
+    }
+}
